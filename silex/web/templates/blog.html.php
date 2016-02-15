c14ec380faeb914a90d2a60dcbcc208db3d5ad15
suckutj <it15093@lehre.dhbw-stuttgart.de>
2016-02-15 15:54:55 +0100

    blog: display postet blogs

--- silex/web/templates/blog.html.php
+++ silex/web/templates/blog.html.php
@@ -4,55 +4,32 @@
  */
 $slots = $view['slots'];
 
-$view->extend('layout.html.php')
+$view->extend('layout.html.php');
 ?>
-
 
 <div id="wrapper" class="container">
     <div class="row">
         <div class="col-12">
-            <div class="jumbotron">
-                <h1>Blog</h1>
-                <br/>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit...
-                <br/>
-                <button id="button_search" type="button" class="btn btn-default" aria-label="Left Align">
-                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                    Search
-                </button>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div id="selectors" class="col-xs-12 col-sm-4">
-            <div class="alert alert-info" role="alert">info box</div>
-            <div class="alert alert-danger" role="alert">danger box</div>
-            <div class="alert alert-warning" role="alert">warning box</div>
-            <div class="alert alert-success" role="alert">success box</div>
-        </div>
-        <div id="article1" class="col-xs-12 col-sm-4">
-            1Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo
-            dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-            sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-            amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-            invidunt ut labore et dolore magna aliquyam erat, sed diam
-            voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-            dolor sit amet.
-        </div>
-        <div id="article2" class="col-xs-12 col-sm-4">
-            2Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo
-            dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-            sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-            amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-            invidunt ut labore et dolore magna aliquyam erat, sed diam
-            voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
-            dolor sit amet.
+            <?php
+            foreach ($content as $dataSet) {
+                ?>
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-info">
+                        <a href="/blog/<?= $dataSet['id'] ?>" alt="blog <?= $dataSet['id'] ?>">
+                            <strong><?= $dataSet['title'] ?></strong>
+                        </a>
+                        <br/>
+                        <em>created by <?php echo $dataSet['author']==NULL ? "(anonym)" : $dataSet['author'] ?> at <?= $dataSet['created_at'] ?></em>
+                    </li>
+                    <li class="list-group-item">
+                        <?php echo $single==true ? nl2br($dataSet['text']) : substr($dataSet['text'], 0, 10) . "   (...)" ?>
+                    </li>
+                </ul>
+                <?php
+            }
+            if ($single) : ?>
+                <a href="/blog" alt="back to blog"><button type="button" class="btn btn-default">back</button></a>
+            <?php endif; ?>
         </div>
     </div>
 </div> <!--wrapper end-->
